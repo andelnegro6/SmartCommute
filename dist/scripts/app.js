c85b71a2db6af686eb3c675276f06383a3443ae7
andelnegro6 <iltoto93@gmail.com>
2018-08-17 19:03:42 +0200

    update2

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
   // page is now ready, initialize the calendar...
 
   $('#calendar').fullCalendar({
+
     // put your options and callbacks here
+    dayClick: function dayClick() {
+      alert('a day has been clicked!');
+    }
   });
 });
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,7 +3,11 @@ $( document ).ready(function() {
   // page is now ready, initialize the calendar...
 
   $('#calendar').fullCalendar({
+
     // put your options and callbacks here
+    dayClick: function() {
+      alert('a day has been clicked!');
+    }
   })
 
 });
