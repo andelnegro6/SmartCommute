e687b3acdabbd0b6016aaec65c5b1212155d4e1a
andelnegro6 <andelnegro6@hotmail.com>
2019-03-06 21:31:25 +0100

    Docs update, presentation

--- test/functiontest.test.js
+++ test/functiontest.test.js
@@ -1,10 +1,10 @@
-const {sum,mul,formato} = require('./funtiontest.js');
+const {sum,mul,formato} = require('./functiontest.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 1 + 2 to equal 3', () => {
+test('multiplies 3 by 3', () => {
   expect(mul(3, 3)).toBe(9);
 });
 
